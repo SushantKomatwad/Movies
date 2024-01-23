0fa9e4d9013dc8d860f17647f21ea858438fc453
SushantKomatwad <136224535+SushantKomatwad@users.noreply.github.com>
2024-01-23 08:49:28 +0530

    first commit

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 const Error = () => {
   return (
     <>
-      
+      <div className='flex flex-col justify-center items-center h-screen space-y-12'>
+        <p className='font-bold text-4xl text-red-600'>
+          Not Found Page
+        </p>
+        <NavLink to='/'>
+          <button className='text-xl font-semibold bg-sky-500 text-white px-2 py-1 rounded-xl'>Go Back</button>
+        </NavLink>
+      </div>
     </>
   )
 }
